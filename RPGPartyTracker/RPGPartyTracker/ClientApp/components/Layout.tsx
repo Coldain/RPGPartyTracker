94868f513e8d73027d70a2b1ffff456b99b423ed
Coldain <wolfpack.ethan@outlook.com>
2018-02-28 20:21:42 -0700

    Added some new "pages"

--- RPGPartyTracker/RPGPartyTracker/ClientApp/components/Layout.tsx
+++ RPGPartyTracker/RPGPartyTracker/ClientApp/components/Layout.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
-import { NavHomeMenu } from './NavMenu/NavHomeMenu';
+import { NavCharacterMenu } from './NavMenu/NavCharacterMenu';
 
 export class Layout extends React.Component<{}, {}> {
     public render() {
         return <div className='container-fluid'>
             <div className='row'>
                 <div className='col-sm-3'>
-                    <NavHomeMenu />
+                    <NavCharacterMenu />
                 </div>
                 <div className='col-sm-9'>
                     { this.props.children }
